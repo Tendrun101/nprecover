517f773069c2689aa9ef4460970c8da4e8499bd1
matengjun <matengjun@huayun.com>
2023-01-11 18:49:32 +0800

    two multi reconciles ways

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,18 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"fmt"
-	"os"
-
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
-
+	"os"
+	"runtime"
 	"sigs.k8s.io/controller-runtime/pkg/builder"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
+	"sigs.k8s.io/controller-runtime/pkg/config/v1alpha1"
+	"strconv"
 
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
@@ -24,7 +26,10 @@ func main() {
 
 	var log = logf.Log.WithName("builder-examples")
 
-	mgr, err := manager.New(config.GetConfigOrDie(), manager.Options{})
+	// 基于此可定义多个goroutine来 Reconcile()
+	mgr, err := manager.New(config.GetConfigOrDie(), manager.Options{
+		Controller: v1alpha1.ControllerConfigurationSpec{GroupKindConcurrency: map[string]int{"ReplicaSet.apps": 3}, CacheSyncTimeout: nil},
+	})
 	if err != nil {
 		log.Error(err, "could not create manager")
 		os.Exit(1)
@@ -34,6 +39,7 @@ func main() {
 		ControllerManagedBy(mgr).  // Create the ControllerManagedBy
 		For(&appsv1.ReplicaSet{}). // ReplicaSet is the Application API
 		Owns(&corev1.Pod{}).       // ReplicaSet owns Pods created by it
+		/////////WithOptions(controller.Options{MaxConcurrentReconciles: 2}).
 		Complete(&ReplicaSetReconciler{})
 	if err != nil {
 		log.Error(err, "could not create controller")
@@ -77,6 +83,7 @@ func (a *ReplicaSetReconciler) Reconcile(ctx context.Context, req reconcile.Requ
 	if err != nil {
 		return reconcile.Result{}, err
 	}
+	fmt.Sprintf("%d goroutine Reconcile ...", GetGID())
 
 	return reconcile.Result{}, nil
 }
@@ -84,4 +91,13 @@ func (a *ReplicaSetReconciler) Reconcile(ctx context.Context, req reconcile.Requ
 func (a *ReplicaSetReconciler) InjectClient(c client.Client) error {
 	a.Client = c
 	return nil
+}
+
+func GetGID() uint64 {
+	b := make([]byte, 64)
+	b = b[:runtime.Stack(b, false)]
+	b = bytes.TrimPrefix(b, []byte("goroutine "))
+	b = b[:bytes.IndexByte(b, ' ')]
+	n, _ := strconv.ParseUint(string(b), 10, 64)
+	return n
 }
